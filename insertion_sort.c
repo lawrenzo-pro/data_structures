035075226affcc30a9054a65221356690c9b7a43
Lawrence Kimani <larrykimani01@gmail.com>
2025-09-24 06:16:55 +0300

    Cleaned up unused functions in insertion sort

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-void swap(int *a, int *b){
-    *a ^= *b;
-    *b ^= *a;
-    *a ^= *b;
-}
 void print_array(int array[],int len){
     for(int i = 0; i < len;i++){
         printf("%d,",array[i]);
--- merge_sort.c
+++ merge_sort.c
@@ -10,6 +10,14 @@ void print_array(int array[],int len){
     }
     printf("\n");
 }
+void merge_sort(int array[],int len){
+
+}
 int main(){
+    int array[] = {-10,0,1,2,3,45,-67,0,9,9,8};
+    int len =  sizeof(array)/ sizeof(int);
+    print_array(array, len);
+    merge_sort(array,len);
+    print_array(array,len);
     return 0;
 }

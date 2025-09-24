088b6d2a7b7b118bfac1948920b0fbd5db47b616
Lawrence Kimani <larrykimani01@gmail.com>
2025-09-24 05:40:26 +0300

    Search algorithms completed

--- binary_search.c
+++ binary_search.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 int binary_search(int value,int array[],int len){
-    return 0;
+    int low = 0;
+    int high = len - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (array[mid] == value)
+            return mid;
+        if (array[mid] < value)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
 }
 int main(){
     int array[] ={0,1,2,3,4,5,6,7,8,9};
     int len = sizeof(array) / sizeof(int);
     int test_1 = 3;
-    int test_2 = 7;
+    int test_2 = 10;
     printf("Is %d in the array?%d\n", test_1, binary_search(test_1,array,len));
     printf("Is %d in the array?%d\n",test_2, binary_search(test_2,array,len));
-    return 0;
     return 0;
 }
--- recursive_binary_search.c
+++ recursive_binary_search.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-int recursive_binary_search(int value, int array[],int len){
-    return 0;
+int recursive_binary_search(int value,int low,int high, int array[],int len){
+    if (high >= low) {
+        int mid = low + (high - low) / 2;
+        if (array[mid] == value)
+            return mid;
+        if (array[mid] > value)
+            return recursive_binary_search(value,low,mid-1,array,len);
+        return recursive_binary_search(value, mid + 1, high, array,len);
+    }
+    return -1;
 }
 int main(){
     int array[] ={0,1,2,3,4,5,6,7,8,9};
     int len = sizeof(array) / sizeof(int);
     int test_1 = 3;
-    int test_2 = 7;
-    printf("Is %d in the array?%d\n", test_1, recursive_binary_search(test_1,array,len));
-    printf("Is %d in the array?%d\n",test_2, recursive_binary_search(test_2,array,len));
+    int test_2 = 17;
+    printf("Is %d in the array?%d\n", test_1, recursive_binary_search(test_1,0,len-1,array,len));
+    printf("Is %d in the array?%d\n",test_2, recursive_binary_search(test_2,0,len-1,array,len));
     return 0;
 }
